0ab616d146693a3937c285d0a0e23f1a59f3853e
Jake Bennett <bennettjj@s.dcsdk12.org>
2015-09-11 09:46:26 -0600

    without doubles finished

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,4 +1,12 @@
-module MethodsQuiz
+module MethodsQuiz2
+
+	def without_doubles(dice1, dice2, doubles)
+		if doubles == true && dice1 == dice2
+			dice1 + dice2 + 1
+		else
+			dice1 + dice2
+		end
+	end 
 	
 	
 
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -11,6 +11,13 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
+	def test_without_doubles
+		assert_equal 9, @m.without_doubles(4, 4, true)
+		assert_equal 8, @m.without_doubles(4, 4, false)
+		assert_equal 3, @m.without_doubles(1, 2, false)
+	end
+
+
 	# TODO - write tests here
 
 end
